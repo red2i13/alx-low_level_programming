5945f2ed96cbe1fcbc9d4fc08d04ef30b105d2a0
RedaSlax <redaben2001@gmail.com>
2023-06-15 19:53:14 -0700

    final

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,5 +12,5 @@ int main(void)
 	printf("Size of a long int: %ld byte(s)\n", sizeof(long));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long));
 	printf("Size of a float: %ld byte(s)\n", sizeof(float));
-	return (0)
+	return (0);
 }
