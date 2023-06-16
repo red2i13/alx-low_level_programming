27104bdba0e76e0b09cddc5098b8118b44f6dbd1
RedaSlax <redaben2001@gmail.com>
2023-06-15 19:48:33 -0700

    final

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Size of char: %zu\n", sizeof(char));
+	printf("Size of int: %zu\n", sizeof(int));
+	printf("Size of long: %zu\n", sizeof(long));
+	printf("Size of long long: %zu\n", sizeof(long long));
+	printf("Size of float: %zu\n", sizeof(float));
+	return (0);
+}
