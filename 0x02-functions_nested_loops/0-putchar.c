ed3d1829fd771b5e2c91ab26f2692a491ae2fe4a
RedaSlax <redaben2001@gmail.com>
2023-06-20 03:07:18 -0700

    my first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Dscripiton: program that print "_putchar" statement
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+
+	int i = 0;
+	char word[] = "_putchar";
+
+	while (word[i] != \0)
+	{
+		putchar(word[i]);
+		i++;
+	}
+		putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory contains C projects about nesting loops and functions
