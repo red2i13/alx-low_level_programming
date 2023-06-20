50ac04765c67034b3d59f09ea516f27eacc24e96
RedaSlax <redaben2001@gmail.com>
2023-06-20 13:55:34 -0700

    my 2nd commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -9,6 +9,7 @@
  */
 
 void print_alphabet(void);
+int _putchar(char c);
 int main(void)
 {
 	print_alphabet();
@@ -32,4 +33,15 @@ void print_alphabet(void)
 	}
 	_putchar('\n');
 
+}
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
