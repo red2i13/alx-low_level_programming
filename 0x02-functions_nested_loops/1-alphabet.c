1e0b77c9b407d3f9ac5d64e78a8d505ddd89a5d3
RedaSlax <redaben2001@gmail.com>
2023-06-20 15:24:54 -0700

    4 fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,6 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * print_alphabet - Entry point
- *
- * Description: function responsible for printing the alphabet
- * Return: Always 0
- */
-
-int main(void)
-{
-	print_alphabet();
-	_putchar('\n');
-	return (0);
-}
-/**
  * print_alphabet - Prints the alphabet in lowercase.
  *
  * Description: This function prints all the lowercase letters of the
@@ -30,15 +16,5 @@ void print_alphabet(void)
 	_putchar(alpha[i]);
 	i++;
 	}
-}
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
+	_putchar('\n');
 }
