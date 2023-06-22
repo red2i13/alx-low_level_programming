0074bc450f0bea8eec9ea121e6de7ac9af9e3b40
RedaSlax <redaben2001@gmail.com>
2023-06-22 14:29:48 -0700

    first commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * _isupper - Entry point
+ * @c: character in ascii
+ *
+ * Description: check if chracter is upper or lower
+ * Return: 1 if uppercase, 0 if lowercase
+ *
+ */
+
+
+int _isupper(int c)
+{
+	if (c > 65 && c < 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
