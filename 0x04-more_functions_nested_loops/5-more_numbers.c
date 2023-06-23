1c0b409726e9d6df35666a3a474179e7cf6c17c7
RedaSlax <redaben2001@gmail.com>
2023-06-23 09:23:58 -0700

    fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,8 +12,12 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 14; j++)
+		for (j = 0; j < 15; j++)
 		{
+			if (j / 10 != 0)
+			{
+				_putchar(j / 10 + '0')
+			}
 			_putchar(j + '0');
 		}
 	_putchar('\n');
