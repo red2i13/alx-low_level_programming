793352e5a80d2cfffb5768808d9e1b599c893906
RedaSlax <redaben2001@gmail.com>
2023-06-26 13:12:26 -0700

    first commmit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry point
+ *
+ * Description: change the value of a variable n using pointers
+ *
+ * @n: memory adrress of variable n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98
+}
