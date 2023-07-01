00c1e57a47cfb72e0d27fe5ffe2d438a6b51cc08
RedaSlax <redaben2001@gmail.com>
2023-06-30 19:21:24 -0700

    nice  fix

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.c"
+
+/**
+ * leet - encode string to 1337
+ * @s: string to encode
+ * Return: pointer to encoded string
+ */
+char *leet(char *s)
+{
+	int i, j;
+
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+			}
+		}
+	}
+	return (s);
+}
