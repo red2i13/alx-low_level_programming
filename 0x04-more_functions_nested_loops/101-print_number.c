04ed196f72dc554adf1cd95f62251740772d42b8
RedaSlax <redaben2001@gmail.com>
2023-08-28 11:51:08 -0700

    hello

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,58 @@
+#include "main.h"
+/**
+  * len - calculate the length of the num
+  * @nb: number
+  * Return: Always i.
+  */
+
+int len(int nb)
+{
+	int i = 0;
+
+	while (nb != 0)
+	{
+		i++;
+		nb /= 10;
+	}
+	return (i);
+}
+/**
+ * print_number - function that prints an integer
+ * @n: number
+ */
+
+void print_number(int n)
+{
+	int i = 1;
+	int loop = len(n);
+	int dev = 1;
+	int num = n;
+
+	while (i < loop)
+	{
+		dev *= 10;
+		i++;
+	}
+	i = 0;
+	if (n > 0)
+	{
+		while (i < loop)
+		{
+			num /= dev;
+			num = num % 10;
+			_putchar(num + '0');
+			num = n;
+			dev /= 10;
+			i++;
+		}
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+		print_number(n);
+	}
+	else
+		_putchar('0');
+
+}
