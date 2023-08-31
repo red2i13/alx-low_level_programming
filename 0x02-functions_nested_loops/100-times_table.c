4bd1ad014357d3099f83d3df97451d9d67b6eec7
RedaSlax <redaben2001@gmail.com>
2023-08-31 10:32:43 -0700

    hello

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * print_times_table - print n tables
+ * @n: n times
+ */
+
+void print_times_table(int n)
+{
+	int row, col;
+	int mult = 0;
+
+	if (n > 15 || n < 0)
+		return;
+	for (row = 0; row <= n; row++)
+	{
+		for (col = 0; col <= n; col++)
+		{
+			mult = (row * col);
+			if (mult < 10)
+			{
+				if (col != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar(mult + '0');
+			}
+			else if (mult < 99)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((mult / 10) + '0');
+				_putchar((mult % 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(mult / 100 + '0');
+				_putchar((mult / 10) % 10 + '0');
+				_putchar((mult % 100) % 10 + '0');
+
+			}
+			if (col != n)
+				_putchar(',');
+		}
+		_putchar('\n');
+	}
+}
