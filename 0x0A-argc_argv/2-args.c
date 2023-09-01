82c96a8911cde5e6a16bfe33e44d930a0f9b839b
RedaSlax <redaben2001@gmail.com>
2023-09-01 16:55:29 -0700

    hello

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+
+/**
+ * main - print all arg
+ * @ac: arg count
+ * @av: arg var
+ * Return: Always 0
+ */
+
+int main(int ac, char **av)
+{
+	int i = 1;
+	int j = 0;
+
+	while (i < ac)
+	{
+		while (av[i][j] != '\0')
+		{
+			write(1, &av[i][j], 1);
+			j++;
+		}
+		write(1, "\n", 1);
+		j = 0;
+		i++;
+	}
+	return (0);
+}
