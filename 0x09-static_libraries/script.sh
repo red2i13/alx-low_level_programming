30433d14e6ea588b40f87a9e695350f27d5594d3
RedaSlax <redaben2001@gmail.com>
2023-09-01 13:21:59 -0700

    hello

--- 0x09-static_libraries/script.sh
+++
@@ -1,7 +0,0 @@
-for file in ./*; do
-    if [ -f "$file" ]; then  # Check if it's a regular file
-        echo "File: $file"
-        cat "$file"  # Display the contents of the file
-        echo  # Add an empty line for separation
-    fi
-done
