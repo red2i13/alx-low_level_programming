3846edea17c20d7a4803707c8ffd925340d259d5
RedaSlax <redaben2001@gmail.com>
2023-09-01 06:39:45 -0700

    hello

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - print array of int
+ * @a: array of int
+ * @n: size of array
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		printf("%i", a[i]);
+		if (i != n - 1)
+			printf(", ");
+		i++;
+	}
+	_putchar('\n');
+}
