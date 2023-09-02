8d9519d7b05badfcc12d3b3bbbfd308827fc8d49
RedaSlax <redaben2001@gmail.com>
2023-09-02 12:07:21 -0700

    hello

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _atoi - ascii to integer
+ * @s: sting
+ * Return: integer
+ */
+
+int _atoi(char *s)
+{
+	int i = 0;
+	int sign = 1;
+	int result = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == ' ')
+		{
+			i++;
+			continue;
+		}
+		else if (s[i] == '-' || s[i] == '+')
+		{
+			if (s[i] == '-')
+				sign *= -1;
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			while (s[i] >= '0' && s[i] <= '9')
+			{
+				result = result * 10 + s[i] - '0';
+				i++;
+			}
+			break;
+		}
+		i++;
+	}
+	return (result * sign);
+}
