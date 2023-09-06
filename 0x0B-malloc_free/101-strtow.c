1304ffb77fbc164202b08e09b9194672ca3a9018
RedaSlax <redaben2001@gmail.com>
2023-09-06 14:35:39 -0700

    hello

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,45 +1,98 @@
 #include "main.h"
 #include <stdlib.h>
 
-char **strtow(char *str)
+/**
+ * _memcpy - replace src to dest with an n limiter of bytes
+ * @dest: the address of memory to print
+ * @src: the byte to write
+ * @start: start of the word
+ * @n: the number of spaces to write
+ * Return: Returns value in dest
+ */
+
+void _memcpy(char *dest, char *src, int start,  int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i + start];
+	}
+	dest[i] = '\0';
+}
+
+/**
+ * _strlen - Entry point
+ *
+ * Description: returns the length of a string
+ *
+ * @str: string of characters
+ *
+ * Return: Always n
+ */
+
+int _strlen(char *str)
 {
 	int i = 0;
-	int j = 0;
-	int l = 0;
-	int k = 0;
-	int len = 0;
-	char **ptr;
+	int words = 0;
+
 	while (str[i] != '\0' && str[i] == ' ')
-	{
 		i++;
-	}
 	while (str[i] != '\0')
 	{
 		if (str[i] == ' ' && (str[i + 1] >= 33 && str[i + 1] <= 126))
-			len++;;
+			words++;
 		i++;
 	}
-	len++;
-	ptr = malloc(sizeof(char *) * len);
+	return (words);
+}
+
+/**
+ * strtow - split string into words
+ * @str: string to split
+ * Return: pointer to words
+ */
+
+char **strtow(char *str)
+{
+	int i = 0, j = 0, len = 0, start, words;
+	char **ptr;
+
+	words = _strlen(str);
+	words += 2;
+	ptr = malloc(sizeof(char *) * words);
+	if (ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
 	i = 0;
-	len = 0;
 	while (str[i] != '\0')
 	{
 		if (str[i] != ' ')
 		{
+			start = i;
 			while (str[i] != ' ')
 			{
 				len++;
 				i++;
 			}
 			ptr[j] = (char *)malloc(sizeof(char) * (len + 1));
+			if (ptr[j] == NULL)
+			{
+				while (j >= 0)
+				{
+					free(ptr[j]);
+					j--;
+				}
+				return (NULL);
+			}
+			_memcpy(ptr[j], str, start,  len);
+			j++;
 		}
-		for (k = 0, l = 0; k <= len; l++, j++)
-			ptr[j][k] = str[l];
-		ptr[i][j] = '\0';
 		len = 0;
 		i++;
-		j++;
 	}
+	ptr[j] = NULL;
 	return (ptr);
 }
