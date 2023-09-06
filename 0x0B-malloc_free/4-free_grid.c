056fe60da9badb093bca3f692150c4472e948fcc
RedaSlax <redaben2001@gmail.com>
2023-09-05 18:29:11 -0700

    hello

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * free_grid - freeup memory
+ * @grid: grid to empty up
+ * @height: second dimension
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	if (grid != NULL && height >= 0)
+	{
+		while (i < height)
+		{
+			free(grid[i]);
+			i++;
+		}
+		free(grid);
+	}
+}
