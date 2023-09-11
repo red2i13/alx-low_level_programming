2990af3e6f3866c3c2ff9933f2883b7b9889a933
RedaSlax <redaben2001@gmail.com>
2023-09-11 08:23:15 -0700

    HELLO

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog {
+/**
+ * struct dog - create a structure for dogs
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: Dog's owner
+ *
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
